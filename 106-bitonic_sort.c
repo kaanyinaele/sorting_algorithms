31047c5043c86dfd62166e8909e618c3f6fbdd5e
kaanyinaele <kaanyinaele@gmail.com>
2024-02-21 23:10:12 -0800

    Bitonic Sort

--- 106-bitonic_sort.c
+++ 106-bitonic_sort.c
@@ -1,95 +1,57 @@
-#include "sort.h"
+/* 106-bitonic_sort.c */
+
 #include <stdio.h>
 #include <stdlib.h>
-
-void bitonic_merge(int *array, size_t start, size_t size, int direction);
-void bitonic_sort_recursive(int *array, size_t start, size_t size, int direction);
-void bitonic_compare(int *array, size_t start, size_t size, int direction);
-void print_array(const int *array, size_t size);
+#include "sort.h"
 
 /**
- * bitonic_sort - Sorts an array of integers in ascending order using Bitonic sort
- * @array: Pointer to the array to sort
+ * bitonic_merge - Merge a bitonic sequence in increasing order
+ * @array: Array to sort
  * @size: Size of the array
- */
+ * @dir: Direction of sorting (1 for increasing, 0 for decreasing)
+ * @sub_size: Size of the sub-array to merge
+ **/
+void bitonic_merge(int *array, size_t size, int dir, size_t sub_size)
+{
+    if (sub_size > 1)
+    {
+        size_t mid = size / 2;
+        size_t i;
+
+        for (i = 0; i < mid; i++)
+        {
+            if ((array[i] > array[i + mid]) == dir)
+            {
+                int temp = array[i];
+                array[i] = array[i + mid];
+                array[i + mid] = temp;
+                printf("Merging [%lu/%lu] (%s):\n", sub_size, size, dir ? "UP" : "DOWN");
+                print_array(array, size);
+            }
+        }
+        bitonic_merge(array, mid, dir, sub_size / 2);
+        bitonic_merge(array + mid, mid, dir, sub_size / 2);
+    }
+}
+
+/**
+ * bitonic_sort - Sorts an array of integers in ascending order
+ *                 using the Bitonic sort algorithm
+ * @array: Array to sort
+ * @size: Size of the array
+ **/
 void bitonic_sort(int *array, size_t size)
 {
-    bitonic_sort_recursive(array, 0, size, 1);
-}
-
-/**
- * bitonic_sort_recursive - Recursively sorts a bitonic sequence
- * @array: Pointer to the array to sort
- * @start: Starting index of the sequence
- * @size: Size of the sequence
- * @direction: Sorting direction (1 for ascending, -1 for descending)
- */
-void bitonic_sort_recursive(int *array, size_t start, size_t size, int direction)
-{
     if (size > 1)
     {
-        size_t half = size / 2;
-        bitonic_sort_recursive(array, start, half, 1);
-        bitonic_sort_recursive(array, start + half, half, -1);
-        bitonic_merge(array, start, size, direction);
-    }
-}
+        size_t mid = size / 2;
 
-/**
- * bitonic_merge - Merges two bitonic sequences
- * @array: Pointer to the array containing the sequences
- * @start: Starting index of the first sequence
- * @size: Size of the sequences
- * @direction: Sorting direction (1 for ascending, -1 for descending)
- */
-void bitonic_merge(int *array, size_t start, size_t size, int direction)
-{
-    if (size > 1)
-    {
-        size_t half = size / 2;
-        bitonic_compare(array, start, size, direction);
-        bitonic_merge(array, start, half, direction);
-        bitonic_merge(array, start + half, half, direction);
-    }
-}
+        printf("Merging [%lu/%lu] (UP):\n", size, size);
+        print_array(array, size);
 
-/**
- * bitonic_compare - Compares and swaps elements in a bitonic sequence
- * @array: Pointer to the array containing the sequence
- * @start: Starting index of the sequence
- * @size: Size of the sequence
- * @direction: Sorting direction (1 for ascending, -1 for descending)
- */
-void bitonic_compare(int *array, size_t start, size_t size, int direction)
-{
-    size_t i, half = size / 2;
-    for (i = start; i < start + half; ++i)
-    {
-        if ((array[i] > array[i + half]) == direction)
-        {
-            int temp = array[i];
-            array[i] = array[i + half];
-            array[i + half] = temp;
-            printf("Merging [%lu/%lu] (%s):\n", size, size, (direction == 1) ? "UP" : "DOWN");
-            print_array(array, start + size);
-        }
+        bitonic_sort(array, mid);
+        bitonic_sort(array + mid, mid);
+        bitonic_merge(array, size, 1, size);
     }
-}
-
-/**
- * print_array - Prints an array of integers
- * @array: Pointer to the array to print
- * @size: Size of the array
- */
-void print_array(const int *array, size_t size)
-{
-    size_t i;
-    for (i = 0; i < size; ++i)
-    {
-        printf("%d", array[i]);
-        if (i < size - 1)
-            printf(", ");
-    }
-    printf("\n");
 }
 
